4d7be14bd9f5b0536adad87d1cdd9314f425a6e9
Yadhul Mohan <yadhulmohan21@gmail.com>
2024-09-28 00:26:51 +0530

    Create program2.c

---
+++ Pratical Programs/Progarm2-UART_Subtractions/program2.c
@@ -0,0 +1,17 @@
+#include <reg51.h>
+#include "uart.h"
+
+void main(){
+	unsigned char num1, num2;
+	SerialInit();
+	
+	Printf("Enter First Number :");
+	num1 = ReadBytePC();
+	Printf("Enter Second Number :");
+	num2 = ReadBytePC();
+	
+	Printf("Subtraction ===> ");
+	SendByte2PC(num1 - num2);
+	while(1){
+	}
+}
