94aab1675cd788caf0bc5e6b3d60f4d3f740fc37
Yadhul Mohan <yadhulmohan21@gmail.com>
2024-10-02 11:46:00 +0530

    Create program3.c

---
+++ Pratical Programs/Program3-UART_Multiplication/program3.c
@@ -0,0 +1,18 @@
+#include <reg51.h>
+#include "uart.h"
+
+void main(){
+	
+	unsigned char num1, num2;
+	SerialInit();
+	
+	Printf("  Enter any number as per your wish: ");
+	num1 = ReadBytePC();
+	Printf("  Enter another number as per your wish: ");
+	num2 = ReadBytePC();
+	Printf("");
+	Printf("  Multiplication ===> ");
+	SendByte2PC(num1 * num2);
+	
+	while(1);
+}
