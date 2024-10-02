937745b8feac5056439c94714c81ddf91b8fc001
Yadhul Mohan <yadhulmohan21@gmail.com>
2024-10-02 12:45:20 +0530

    Create Program5.c

---
+++ Pratical Programs/Program5-UART_Add_Or_Sub/Program5.c
@@ -0,0 +1,43 @@
+#include <reg51.h>
+#include "uart.h"
+
+sbit sw = P1^0;
+unsigned char current_sw_state;
+unsigned char num1, num2;
+
+void ask(){
+	Printf("  Enter any number as per your wish: ");
+	num1 = ReadBytePC();
+	Printf(" Enter another number as per your wish: ");
+	num2 = ReadBytePC();
+	Printf("");
+}
+
+void main(){
+	
+	SerialInit();
+	sw = 0;
+	
+	ask();
+	
+	while(1){
+	if(sw == 0){
+		Printf(" Addition ==> ");
+		SendByte2PC(num1+num2);
+	}
+	else{
+		Printf(" Subtraction ==> ");
+		SendByte2PC(num1 - num2);
+	}
+	
+	Printf("");
+	Printf("Switch 'ON' state ===> Addition ");
+	Printf("Switch 'OFF' state ===> Subtraction ");
+	Printf("Change the mode of the switch ");
+	
+	while( sw == current_sw_state);
+	ask();
+	
+ }
+	
+}
