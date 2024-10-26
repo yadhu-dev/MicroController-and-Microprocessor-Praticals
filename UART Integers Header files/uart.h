068d8a97f61a49eb35e5c719d52c79d15dc8f7d2
Yadhul Mohan <yadhulmohan21@gmail.com>
2024-10-26 21:57:43 +0530

    Delete UART Integers Header files/uart.h

--- UART Integers Header files/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef UART_INT_H
-#define UART_INT_H
-
-// Function Prototypes
-void SerialInit(void);
-void serial(unsigned char x);
-void Printf(unsigned char s[]);
-unsigned int ReadIntPC(void);
-void SendInt2PC(unsigned int value);
