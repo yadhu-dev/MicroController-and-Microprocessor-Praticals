9e6b9a7d185af0b7477b2c1b32f7b3a3f56881d1
Yadhul Mohan <yadhulmohan21@gmail.com>
2024-10-28 23:28:21 +0530

    Create program19.c

---
+++ Pratical Programs/Program19_Up and Down Counter/program19.c
@@ -0,0 +1,41 @@
+#include <reg51.h>
+#include "sev_segment.h"
+
+#define SEG P3
+
+void delay(unsigned int time){
+	unsigned int i,j;
+	for(i=0;i< time; i++){
+		for(j=0;j<1275;j++);
+	}
+}
+
+void up_counter(){
+	unsigned char count;
+		for(count = 0;count <=9;count++){
+			SEG = seg_display(count);
+			delay(200);
+			if(count == 9){
+				break;
+			}
+		}
+	}
+
+void down_counter(){
+	unsigned char count;
+	
+	for(count=9; count <= 9; count--){
+		SEG = seg_display(count);
+		delay(200);
+		if(count == 0){
+			up_counter();
+			count = 10;
+		}
+	}
+}
+
+void main(){
+	while(1){
+		down_counter();
+	}
+}
